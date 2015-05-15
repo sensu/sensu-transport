f1af1a5a975577d34c4ba96980aee5f3b4472a6c
Sean Porter <portertech@gmail.com>
2015-05-15 13:24:45 -0700

    [redis-transport] first pass at redis transport, most tests are passing

--- lib/sensu/transport/redis.rb
+++ lib/sensu/transport/redis.rb
@@ -1,132 +1,121 @@
+gem "em-redis-unified"
+
+require "em-redis"
+
 require File.join(File.dirname(__FILE__), "base")
 
 module Sensu
   module Transport
     class Redis < Base
-      # @!attribute [rw] logger
-      #   @return [Logger] the Sensu logger object.
-      attr_accessor :logger
 
-      def initialize
-        @on_error = Proc.new {}
-        @before_reconnect = Proc.new {}
-        @after_reconnect = Proc.new {}
+      REDIS_KEYSPACE = "transport:"
+
+      def connect(options={})
+        reset
+        @redis = EM::Protocols::Redis.connect(options)
+        @pubsub = EM::Protocols::Redis.connect(options)
       end
-
-      # Set the error callback.
-      #
-      # @param callback [Proc] called in the event of a transport
-      #   error, the exception object should be passed as a parameter.
-      # @return [Proc] the error callback.
-      def on_error(&callback)
-        @on_error = callback
-      end
-
-      # Set the before reconnect callback.
-      #
-      # @param callback [Proc] called before attempting to reconnect
-      #   to the transport.
-      # @return [Proc] the before reconnect callback.
-      def before_reconnect(&callback)
-        @before_reconnect = callback
-      end
-
-      # Set the after reconnect callback.
-      #
-      # @param callback [Proc] called after reconnecting to the
-      #   transport.
-      # @return [Proc] the after reconnect callback.
-      def after_reconnect(&callback)
-        @after_reconnect = callback
-      end
-
-      # Transport connection setup.
-      #
-      # @param options [Hash, String]
-      def connect(options={}); end
 
       # Reconnect to the transport.
       def reconnect; end
 
-      # Indicates if connected to the transport.
-      #
-      # @return [TrueClass, FalseClass]
       def connected?
-        false
+        @redis.connected? && @pubsub.connected?
       end
 
-      # Close the transport connection.
-      def close; end
+      def close
+        @redis.close
+        @pubsub.close
+      end
 
-      # Publish a message to the transport.
-      #
-      # @param type [Symbol] the transport pipe type, possible values
-      #   are: :direct and :fanout.
-      # @param pipe [String] the transport pipe name.
-      # @param message [String] the message to be published to the transport.
-      # @param options [Hash] the options to publish the message with.
-      # @yield [info] passes publish info to an optional callback/block.
-      # @yieldparam info [Hash] contains publish information, which
-      #   may contain an error object.
       def publish(type, pipe, message, options={}, &callback)
-        info = {:error => nil}
-        callback.call(info) if callback
-      end
-
-      # Subscribe to a transport pipe and/or funnel.
-      #
-      # @param type [Symbol] the transport pipe type, possible values
-      #   are: :direct and :fanout.
-      # @param pipe [String] the transport pipe name.
-      # @param funnel [String] the transport funnel, which may be
-      #   connected to multiple pipes.
-      # @param options [Hash] the options to consume messages with.
-      # @yield [info, message] passes message info and content to
-      #   the consumer callback/block.
-      # @yieldparam info [Hash] contains message information.
-      # @yieldparam message [String] message.
-      def subscribe(type, pipe, funnel=nil, options={}, &callback)
-        info = {}
-        message = ''
-        callback.call(info, message)
-      end
-
-      # Unsubscribe from all transport pipes and/or funnels.
-      #
-      # @yield [info] passes info to an optional callback/block.
-      # @yieldparam info [Hash] contains unsubscribe information.
-      def unsubscribe(&callback)
-        info = {}
-        callback.call(info) if callback
-      end
-
-      # Acknowledge the delivery of a message from the transport.
-      #
-      # @param info [Hash] message information, eg. contains its id.
-      # @yield [info] passes acknowledgment info to an optional callback/block.
-      def acknowledge(info, &callback)
-        callback.call(info) if callback
-      end
-
-      # Alias for acknowledge()
-      def ack(*args, &callback)
-        acknowledge(*args, &callback)
-      end
-
-      # Transport funnel stats, such as message and consumer counts.
-      #
-      # @param funnel [String] the transport funnel to get stats for.
-      # @param options [Hash] the options to get funnel stats with.
-      def stats(funnel, options={}, &callback)
-        info = {}
-        callback.call(info)
-      end
-
-      # Discover available transports (Subclasses)
-      def self.descendants
-        ObjectSpace.each_object(Class).select do |klass|
-          klass < self
+        case type
+        when "fanout"
+          pubsub_publish(pipe, message, &callback)
+        when "direct"
+          list_publish(pipe, message, &callback)
         end
+      end
+
+      def subscribe(type, pipe, funnel=nil, options={}, &callback)
+        case type
+        when "fanout"
+          pubsub_subscribe(pipe, &callback)
+        when "direct"
+          list_subscribe(pipe, &callback)
+        end
+      end
+
+      def unsubscribe(&callback)
+        @pubsub.unsubscribe do
+          @logger.debug("unsubscribed from all redis channels") if @logger
+          callback.call({}) if callback
+        end
+        @subscribed = {}
+      end
+
+      def stats(funnel, options={}, &callback)
+        @redis.llen(funnel) do |messages|
+          info = {
+            :messages => messages,
+            :consumers => 0
+          }
+          callback.call(info)
+        end
+      end
+
+      private
+
+      def reset
+        @subscribed = {}
+      end
+
+      def pubsub_publish(pipe, message, &callback)
+        channel = REDIS_KEYSPACE + pipe
+        @redis.publish(channel, message) do |subscribers|
+          info = {:subscribers => subscribers}
+          callback.call(info) if callback
+        end
+      end
+
+      def pubsub_subscribe(pipe, &callback)
+        channel = REDIS_KEYSPACE + pipe
+        @pubsub.subscribe(channel) do |type, channel, message|
+          case type
+          when "subscribe"
+            @logger.debug("subscribed to redis channel", :channel => channel) if @logger
+            @subscribed[channel] = true
+          when "unsubscribe"
+            @logger.debug("unsubscribed from redis channel", :channel => channel) if @logger
+            @subscribed.delete(channel)
+          when "message"
+            info = {:channel => channel}
+            callback.call(info, message)
+          end
+        end
+      end
+
+      def list_publish(pipe, message, &callback)
+        list = REDIS_KEYSPACE + pipe
+        @redis.rpush(list, message) do |queued|
+          info = {:queued => queued}
+          callback.call(info) if callback
+        end
+      end
+
+      def list_lpop(list, &callback)
+        @redis.lpop(list) do |message|
+          if @subscribed[list]
+            EM::next_tick {list_lpop(list, &callback)}
+          end
+          callback.call({}, message) unless message.nil?
+        end
+      end
+
+      def list_subscribe(pipe, &callback)
+        list = REDIS_KEYSPACE + pipe
+        @subscribed[list] = true
+        list_lpop(list, &callback)
       end
     end
   end
---
+++ spec/redis_spec.rb
@@ -0,0 +1,97 @@
+require File.join(File.dirname(__FILE__), "helpers")
+require "sensu/transport/redis"
+require "logger"
+
+describe "Sensu::Transport::Redis" do
+  include Helpers
+
+  before do
+    @transport = Sensu::Transport::Redis.new
+    @transport.logger = Logger.new(STDOUT)
+    @transport.logger.level = Logger::FATAL
+  end
+
+  it "provides a transport API" do
+    expect(@transport).to respond_to(:on_error, :before_reconnect, :after_reconnect,
+                                     :connect, :reconnect, :connected?, :close,
+                                     :publish, :subscribe, :unsubscribe,
+                                     :acknowledge, :ack, :stats)
+  end
+
+  it "can publish and subscribe" do
+    async_wrapper do
+      @transport.connect
+      callback = Proc.new do |info, message|
+        expect(info).to be_kind_of(Hash)
+        expect(message).to eq("msg")
+        timer(0.5) do
+          async_done
+        end
+      end
+      @transport.subscribe("direct", "foo", "baz", {}, &callback)
+      @transport.subscribe("direct", "bar", "baz", {}, &callback)
+      timer(1) do
+        @transport.publish("direct", "foo", "msg") do |info|
+          expect(info).to be_kind_of(Hash)
+          expect(info[:queued]).to eq(1)
+        end
+      end
+    end
+  end
+
+#  it "can unsubscribe from queues and close the connection" do
+#    async_wrapper do
+#      @transport.connect
+#      @transport.subscribe("direct", "bar") do |info, message|
+#        true
+#      end
+#      timer(1) do
+#        @transport.unsubscribe do
+#          @transport.close
+#          expect(@transport.connected?).to be(false)
+#          async_done
+#        end
+#      end
+#    end
+#  end
+
+  it "can open and close the connection immediately" do
+    async_wrapper do
+      @transport.connect
+      @transport.close
+      timer(1) do
+        expect(@transport.connected?).to be(false)
+        async_done
+      end
+    end
+  end
+
+  it "can get queue stats, message and consumer counts" do
+    async_wrapper do
+      @transport.connect
+      @transport.stats("bar") do |info|
+        expect(info).to be_kind_of(Hash)
+        expect(info[:messages]).to eq(0)
+        expect(info[:consumers]).to eq(0)
+        async_done
+      end
+    end
+  end
+
+#  it "can fail to connect" do
+#    async_wrapper do
+#      @transport.connect(:port => 5555)
+#      expect(@transport.connected?).to be(false)
+#      async_done
+#    end
+#  end
+
+  it "will throw an error if it cannot resolve a hostname" do
+    async_wrapper do
+      expect {
+        @transport.connect(:host => "2def33c3-cfbb-4993-b5ee-08d47f6d8793")
+      }.to raise_error
+      async_done
+    end
+  end
+end
